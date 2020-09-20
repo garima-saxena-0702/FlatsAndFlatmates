819edb998a22c7d18a31b8b07017f1e0182f36c6
Garima Saxena <garimasaxena0702@gmail.com>
2020-09-21 00:47:55 +0530

    Added login anactive

--- flatsAndFlatmatesFrontend/src/app/login-page/login-page.component.ts
+++ flatsAndFlatmatesFrontend/src/app/login-page/login-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  public newUser: boolean = false;
+
   constructor() { }
   
   ngOnInit() {
